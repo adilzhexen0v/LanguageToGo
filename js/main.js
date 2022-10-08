39823cb6dd6d32b00a20905fdd2f0b086c01b321
Adil Zhexenov <nescrypnoadil@gmail.com>
2022-10-08 14:39:07 +0600

    add testimonials (home)

--- js/main.js
+++ js/main.js
@@ -17,4 +17,20 @@ window.addEventListener('DOMContentLoaded', function(){
 
           }
      });
+
+     /* Tabs */
+     const tabsBtns = document.querySelectorAll('.testimonials__slider__dot');
+     const tabsLists = document.querySelectorAll('.testimonials__slider__item');
+     tabsBtns.forEach((element, index) => {
+          element.addEventListener('click', function(){
+               tabsBtns.forEach((element, i) => {
+                    element.classList.remove('active__dot');
+                    tabsLists[i].classList.remove('show', 'fade');
+               });
+               element.classList.add('active__dot');
+               tabsLists[index].classList.add('show', 'fade');
+          });
+     });
+     
+
 });
